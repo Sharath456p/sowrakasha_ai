5b19d85d5292f93eb680d94d8b54e518e3cd0003
Sharath Kumar KP <sharathkumarkp@Mac.lan>
2026-02-04 01:12:31 +0400

    feat(infra): reserve global static IP for sowrakasha.me

---
+++ infrastructure/ip.tf
@@ -0,0 +1,3 @@
+resource "google_compute_global_address" "ingress_ip" {
+  name = "sowrakasha-ingress-ip"
+}
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -6,7 +6,12 @@ output "cluster_location" {
   value = google_container_cluster.primary.location
 }
 
-output "cluster_endpoint" {
-  value     = google_container_cluster.primary.endpoint
-  sensitive = true
+output "kubernetes_cluster_host" {
+  value       = google_container_cluster.primary.endpoint
+  description = "GKE Cluster Host"
+}
+
+output "ingress_ip_address" {
+  value       = google_compute_global_address.ingress_ip.address
+  description = "The static global IP address for the Ingress (Point Cloudflare here)"
 }
